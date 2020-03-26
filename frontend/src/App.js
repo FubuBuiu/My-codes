a4920fb83ccc7986062e76ec0707bcecf8a43c9d
FUBU <brendonangelo@hotmail.com>
2020-03-25 23:42:07 -0300

    Front-end da aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import {useState} from 'react'
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
+
+  // const [counter,setCounter] = useState(0);
+  // //O useState retorna um array [valor, funcaoDeAtual]
+  // function increment(){
+  //   setCounter(counter + 1);
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Marcos, Omnistack na área!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 

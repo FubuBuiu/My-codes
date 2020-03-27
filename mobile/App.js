1a68a38d6c0b47e5a2721b370538658a123955cc
FUBU <brendonangelo@hotmail.com>
2020-03-26 21:28:17 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,16 @@
+
+
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+
+//Tag VIEW são os containers como DIV
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
